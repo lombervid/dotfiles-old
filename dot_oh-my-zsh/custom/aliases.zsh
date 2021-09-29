24ba6b83bf717b1c8805ac36eac03a5d0f2ba05f
lombervid <lombervid@localhost.localdomain>
2021-09-29 15:23:53 -0500

    Add ~/.oh-my-zsh/custom/aliases.zsh file

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,12 @@
+# Zsh
+alias refresh='source ~/.zshrc'
+alias zshconfig='nvim ~/.zshrc'
+alias zshalias='nvim ~/.oh-my-zsh/custom/aliases.zsh'
+
+# Powelevel10k
+alias p10ksettings='nvim ~/.p10k.zsh'
+
+# Batcat
+alias cat='bat  --paging=never'
+alias catp='bat  --paging=never -p'
+alias catn='bat  --paging=never -n'
